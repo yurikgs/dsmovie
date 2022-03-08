9324aaff814ab5b3d2055213f05a62da0aef5859
yurikgs <yurikgs@outlook.com>
2022-03-08 09:56:06 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,19 +9,19 @@ function Listing() {
 
         <div className="container mt-5">
           <div className="row">
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">              
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
                <MovieCard/>
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">              
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
                <MovieCard/>
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">              
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
                <MovieCard/>
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">              
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
                <MovieCard/>
             </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">              
+            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
                <MovieCard/>
             </div>
           </div>
