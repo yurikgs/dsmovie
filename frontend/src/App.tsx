a46684f3cca2305bf73cad08d3f09f7629df069f
yurikgs <yurikgs@outlook.com>
2022-03-08 00:29:49 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import Navbar from "./components/navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 
 function App() {
   return (
-
+    <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing/>}></Route>
+        <Route path="/form">
+           <Route path=":movieId" element={<Form/>} ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      
 
   )
 }
