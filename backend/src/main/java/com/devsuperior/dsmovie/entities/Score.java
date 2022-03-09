35baf9c2950a0d602c25668e1b8f9d28f484b65a
yurikgs <yurikgs@outlook.com>
2022-03-09 09:42:43 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_score")
+public class Score {
+
+	@EmbeddedId
+	public ScorePK id = new ScorePK();
+	
+	
+	private Double value;
+	
+	public Score(){
+	}
+	
+	
+	
+	
+	//Setar o movie da ScorePK
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	//Setar o user da ScorePK
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+
+	//Getters and Setters
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	
+	
+}
