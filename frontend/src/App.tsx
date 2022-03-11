9891d2389f4799bb0d991cdfc3591fffbd552f50
yurikgs <yurikgs@outlook.com>
2022-03-10 22:34:51 -0300

    bugfix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,13 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Navbar from "components/Navbar";
+// import Navbar from "components/Navbar";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Listing/>}></Route>
         <Route path="/form">
