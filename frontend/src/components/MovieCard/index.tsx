9d9e3461b0414b4712340d3146ab0491a14349ac
yurikgs <yurikgs@outlook.com>
2022-03-12 19:27:52 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -3,20 +3,12 @@ import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 
 
-// type Props = {
-//   movie: Movie
-// }
+type Props = {
+  movie: Movie
+}
 
-// { movie }: Props
-function MovieCard() {
 
-  const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5
-};
+function MovieCard({ movie }: Props) {
 
   return (
     <div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,50 +3,56 @@ import MovieCard from "components/MovieCard"
 import Pagination from "components/Pagination"
 import { useEffect, useState } from "react"
 import { BASE_URL } from "utils/requests"
-import { MoviePage } from "types/movie"
+import { Movie, MoviePage } from "types/movie"
 
 function Listing() {
 
-   const [pageNumber, setPageNumber] = useState(0)
+   const [pageNumber, setPageNumber] = useState(1)
+
+   const [page, setPage] = useState<MoviePage>({      
+   content: [],
+   last: true,
+   totalPages: 0,
+   totalElements: 0,
+   size: 0,
+   number: 0,
+   first: true,
+   numberOfElements: 0,
+   empty: true
+   })
+
 
    useEffect(() => {
-      axios.get(`${BASE_URL}/movies?size=12&page=0`)
+      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
       .then(response => {
          const data = response.data as MoviePage
-         setPageNumber(data.number)
-         console.log(data)
+         setPage(data)
       })
-   }, [])
-
-
+   }, [pageNumber])
+  
 
   return (
     <>
-    <p>{pageNumber}</p>
     <Pagination/>
 
         <div className="container mt-5">
           <div className="row">
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
-               <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
-               <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
-               <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
-               <MovieCard/>
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
-               <MovieCard/>
-            </div>
+    {/* Renderização dinâmica de coleção abaixo (via .map):
+         * Precisa conter o atributo "key", e este deve ser algum atributo único de cada elemento renderizado. (Característica do React)
+    */}
+
+             {page.content.map( movie => (
+                 <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-4">              
+                    <MovieCard movie = {movie}/>
+                 </div>
+             ))}
+            
           </div>
         </div>
-
+ 
     </>
     
   )
 } 
+
 export default Listing
